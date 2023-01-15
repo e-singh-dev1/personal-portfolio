ce82f5a797dd81e16a65a12b4f2579a5ef75f131
Eshan Singh <eshansingh510@gmail.com>
2023-01-16 02:00:48 +0530

    10th commit  Changes to be committed: 	modified:   src/Components/Contact.jsx 	modified:   src/Components/Skills.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <div name='Contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center  p-4 md:flex md:flex-col sm:flex sm:flex-col sm:justify-center sm:items-center bg-no-repeat bg-center sm:bg-no-repeat sm:bg-cover'
       style={{backgroundImage: `url(${img5})`}}>
         <div className='flex justify-items-end mt-10 items-center sm:flex sm:justify-center sm:items-center'>
-        <form action="https://getform.io/f/6b53a6dd-21ea-4a1d-a0bd-bf8cac291b42" method="POST" className='flex flex-col w-[400px] w-full sm:w-full sm:w-[600px] mt-10 '>
+        <form action="https://getform.io/f/6b53a6dd-21ea-4a1d-a0bd-bf8cac291b42" method="POST" className='flex flex-col w-[200px] w-full sm:w-full sm:w-[600px] mt-10 '>
             <div className='flex flex-col justify-center items-center sm:flex sm:flex-col sm:justify-center sm:items-center pb-4 sm:pb-8'>
                 <p className='text-4xl sm:text-6xl font-bold flex m-0 border-b-4 border-pink-600 text-gray-300 md:flex md:flex-col sm:flex sm:flex-col '>
                     Contact
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -11,12 +11,12 @@ import ReactImg from '../assets/react.png'
 
 const Skills = () => {
   return (
-    <div name='skills' className='m-0 sm:w-full sm:h-screen bg-[#0a192f] text-gray-300 md:flex md:flex-col sm:flex sm:flex-col sm:m-0 '>
+    <div name='skills' className='sm:w-full sm:h-screen bg-[#0a192f] text-gray-300 md:flex md:flex-col sm:flex sm:flex-col sm:m-0 '>
       {/* Container */}
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-          <div className='flex flex-col'>
-              <p className='text-2xl sm:text-6xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
-              <p className='p-10 text-sm font-bold sm:text-xl sm:font-sans sm:py-4'>// These are the technologies I've worked with:</p>
+      <div className='max-w-[1000px] sm:mx-auto p-4 flex flex-col justify-center w-full h-full'>
+          <div className='flex flex-col mt-14'>
+              <p className='text-4xl sm:text-6xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
+              <p className='p-4 text-sm font-bold sm:text-xl sm:font-sans sm:py-4'>// These are the technologies I've worked with:</p>
           </div>
 
           <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
